e80fc364f920ae45b97e117a672bf632b44f9035
SebChia <104203508+SebChia@users.noreply.github.com>
2023-04-09 09:44:50 +0800

    Update ViewFWAAnalytics.java

--- bit203Assignment2/src/bit203Assignment2/ViewFWAAnalytics.java
+++ bit203Assignment2/src/bit203Assignment2/ViewFWAAnalytics.java
@@ -30,14 +30,10 @@ public class ViewFWAAnalytics extends JFrame {
 	private static JTable table;
 	private static JScrollPane scrollPane;
 
-	FWARequest fr1_1 = new FWARequest("R001","2023-03-01", "Work-from-home", "Can set for 3 days?", "Not well", "Accepted", "Understood, take care");
-	FWARequest fr1_2 = new FWARequest("R002","2023-03-07", "Flexi-Hours", "I can work for 1 week at office", "Home Wi-Fi not working", "Pending", "");
-	FWARequest fr2_1 = new FWARequest("R003","2023-03-01", "Hybrid", "May I for 1 month?", "-", "Rejected", "Reason must be given");
-	FWARequest fr2_2 = new FWARequest("R004","2023-03-08", "Flexi-Hours", "I can do for 3 weeks", "None", "Pending", "");
-	FWARequest fr3_1 = new FWARequest("R005","2023-02-27", "Work-from-home", "-", "-", "Rejected", "");
-	FWARequest fr3_2 = new FWARequest("R006","2023-03-08", "Hybrid", "Can set until changes are needed?", "Preferred", "Pending", "");
-	FWARequest fr4_1 = new FWARequest("R007","2023-03-01", "Work-from-home", "Need 3 days", "Need to take care of son", "Accepted", "");
-	FWARequest fr4_2 = new FWARequest("R008","2023-03-08", "Work-from-home", "Can set for 3 days?", "Not feeling well", "Pending", "");
+	FWARequest fr1_1 = new FWARequest("R001","2023-04-09", "Work-from-home", "Can set for 3 days?", "Not well", "Pending", "U");
+	FWARequest fr2_1 = new FWARequest("R002","2023-04-09", "Hybrid", "May I for 1 month?", "-", "Pending", "");
+	FWARequest fr3_1 = new FWARequest("R003","2023-04-09", "Hybrid", "-", "-", "Pending", "");
+	FWARequest fr4_1 = new FWARequest("R004","2023-04-09", "Flexi-Hours", "Need 3 days", "Can work in office", "Pending", "");
 	
 	// Employee objects
 		Employee em1 = new Employee("E100","IamAdam","Adam","Accountant","adam246@gmail.com","Work-from-home");
@@ -104,13 +100,9 @@ public class ViewFWAAnalytics extends JFrame {
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{em1.getEmployeeID(),fr1_1.getRequestID(),fr1_1.getRequestDate(),fr1_1.getWorkType(),fr1_1.getDescription(),fr1_1.getReason(),fr1_1.getStatus(),fr1_1.getComment()},
-				{em1.getEmployeeID(),fr1_2.getRequestID(),fr1_2.getRequestDate(),fr1_2.getWorkType(),fr1_2.getDescription(),fr1_2.getReason(),fr1_2.getStatus(),fr1_2.getComment()},
 				{em2.getEmployeeID(),fr2_1.getRequestID(),fr2_1.getRequestDate(),fr2_1.getWorkType(),fr2_1.getDescription(),fr2_1.getReason(),fr2_1.getStatus(),fr2_1.getComment()},
-				{em2.getEmployeeID(),fr2_2.getRequestID(),fr2_2.getRequestDate(),fr2_2.getWorkType(),fr2_2.getDescription(),fr2_2.getReason(),fr2_2.getStatus(),fr2_2.getComment()},
 				{em3.getEmployeeID(),fr3_1.getRequestID(),fr3_1.getRequestDate(),fr3_1.getWorkType(),fr3_1.getDescription(),fr3_1.getReason(),fr3_1.getStatus(),fr3_1.getComment()},
-				{em3.getEmployeeID(),fr3_2.getRequestID(),fr3_2.getRequestDate(),fr3_2.getWorkType(),fr3_2.getDescription(),fr3_2.getReason(),fr3_2.getStatus(),fr3_2.getComment()},
 				{em4.getEmployeeID(),fr4_1.getRequestID(),fr4_1.getRequestDate(),fr4_1.getWorkType(),fr4_1.getDescription(),fr4_1.getReason(),fr4_1.getStatus(),fr4_1.getComment()},
-				{em4.getEmployeeID(),fr4_2.getRequestID(),fr4_2.getRequestDate(),fr4_2.getWorkType(),fr4_2.getDescription(),fr4_2.getReason(),fr4_2.getStatus(),fr4_2.getComment()},
 				},
 			new String[] {
 				"Employee ID", "Request ID", "Request Date", "Work Type", "Description", "Reason", "Status", "Supervisor comments"
