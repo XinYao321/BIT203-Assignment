c11f57f1e7d2219e58b0ed80bd73415c749e59d3
XinYao321 <xinyaolee01@gmail.com>
2023-04-09 12:31:48 +0800

    Commit and push

---
+++ bit203Assignment2/src/bit203Assignment2/TableModel.java
@@ -0,0 +1,51 @@
+package bit203Assignment2;
+// Date: 7th April 2023
+
+/*Table Model class*/
+
+// @author: Lee Xin Yao B2100617
+public class TableModel {
+	private Object[][] data = {};
+	  private String[] columnNames = {};
+	  private Class[] columnClass = {};
+	  private Object[][] rowData = new Object[][] {};
+	
+	public TableModel() {
+	  }
+	
+	public int getRowCount() {
+	    return rowData.length;
+	  }
+
+	  public int getColumnCount() {
+	    return columnNames.length;
+	  }
+
+	  public String getColumnName(int columnIndex) {
+	    return columnNames[columnIndex];
+	  }
+
+	  
+	  public Class getColumnClass(int columnIndex) {
+	    return columnClass[columnIndex];
+	  }
+
+	  
+	  public boolean isCellEditable(int rowIndex, int columnIndex) {
+	    boolean isEditable = true;
+	    if (columnIndex == 0) {
+	      isEditable = false; // Make the ID column non-editable
+	    }
+	    return isEditable;
+	  }
+
+	  
+	  public Object getValueAt(int rowIndex, int columnIndex) {
+	    return rowData[rowIndex][columnIndex];
+	  }
+
+	  
+	  public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+	    rowData[rowIndex][columnIndex] = aValue;
+	  }
+}
